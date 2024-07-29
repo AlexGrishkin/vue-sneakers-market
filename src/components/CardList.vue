f06f2c4a57d6d671a7d6b767413d79ce1cf3d7e8
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2024-07-29 16:35:59 +0300

    Начальная верстка для ответвления

---
+++ src/components/CardList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="5000"
+    /><Card /><Card /><Card /><Card /><Card /><Card />
+  </div>
+</template>
