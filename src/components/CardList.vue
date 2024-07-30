9837f4ce62806c0e4eb86dce7f9c17b78272a769
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2024-07-30 12:24:47 +0300

    Создан элемент для корзины и карточки внутри корзины

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,12 +1,17 @@
 <script setup>
 import Card from './Card.vue'
+const onClickAdd = () => {
+  alert('Добавить')
+}
 </script>
+
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
       imageUrl="/sneakers/sneakers-1.jpg"
       title="Мужские Кроссовки Nike Blazer Mid Suede"
       :price="5000"
-    /><Card /><Card /><Card /><Card /><Card /><Card />
+      :onClickAdd="onClickAdd"
+    />
   </div>
 </template>
